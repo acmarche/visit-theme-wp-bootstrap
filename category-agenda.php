9519a42195c851fbb5604a866f9a04b482af9ce7
jfsenechal <jfsenechal@gmail.com>
2021-02-26 17:38:23 +0100

    page events

---
+++ category-agenda.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AcMarche\Theme;
+
+use AcMarche\Common\Twig;
+use AcMarche\Pivot\Repository\HadesRepository;
+use Psr\Cache\InvalidArgumentException;
+
+get_header();
+
+$hadesRepository = new HadesRepository();
+try {
+    $events = $hadesRepository->getEvents();
+} catch (InvalidArgumentException $e) {
+    Twig::rendPage(
+        'errors/500.html.twig',
+        [
+            'message' => 'Impossible de charger les évènements: '.$e->getMessage(),
+        ]
+    );
+    get_footer();
+
+    return;
+}
+
+Twig::rendPage(
+    'agenda/index.html.twig',
+    [
+        'events' => $events,
+    ]
+);
+
+get_footer();
