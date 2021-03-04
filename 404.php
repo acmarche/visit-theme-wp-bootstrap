b8490944e086800e98afbe21a722cd542e0c9a44
jfsenechal <jfsenechal@gmail.com>
2021-03-04 14:13:16 +0100

    details

--- 404.php
+++ 404.php
@@ -2,18 +2,22 @@
 
 namespace AcMarche\Theme;
 
+use AcMarche\Common\Router;
 use AcMarche\Common\Twig;
 use VisitMarche\Theme\Inc\CategoryMetaBox;
 use VisitMarche\Theme\Lib\WpRepository;
 
 get_header();
 global $wp_rewrite;
-dump($wp_rewrite);
+
+//dump($wp_rewrite);
+$url = Router::getCurrentUrl();
 Twig::rendPage(
     'errors/404.html.twig',
     [
         'title' => '404',
         'posts' => [],
+        'url' => $url,
     ]
 );
 
