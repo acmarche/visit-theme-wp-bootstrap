5ae7a686306c200b408dcdccaa368a336c7c9c61
jfsenechal <jfsenechal@gmail.com>
2021-04-07 14:12:04 +0200

    cats

--- 404.php
+++ 404.php
@@ -3,18 +3,54 @@
 namespace AcMarche\Theme;
 
 use AcMarche\Common\Router;
+use AcMarche\Pivot\Repository\HadesRemoteRepository;
+use AcMarche\Pivot\Repository\HadesRepository;
+use AcMarche\Pivot\Utils\CategoryUtils;
 use VisitMarche\Theme\Lib\Twig;
+use VisitMarche\Theme\Lib\WpRepository;
 
 get_header();
 
-$url = Router::getCurrentUrl();
-Twig::rendPage(
-    'errors/404.html.twig',
-    [
-        'title' => '404',
-        'posts' => [],
-        'url' => $url,
-    ]
-);
+$hadesRemoteRepository = new HadesRemoteRepository();
+$categoryUtils = new CategoryUtils();
+$tree = [];
+$first = true;
+$lvl2 = $lvl3 = false;
+$categoryUtils->initLvl();
+foreach ($categoryUtils->categories as $category) {
+
+    if ($category->lvl1) {
+        if ($lvl2) {
+            $categoryUtils->finishLvl();
+            dump($categoryUtils->lvl);
+            $categoryUtils->initLvl();
+            $lvl2 = false;
+        }
+        $categoryUtils->lvl['name'] = $category->lvl1;
+    }
+
+    if ($category->lvl2) {
+        $lvl2 = true;
+        if ($lvl3) {
+            $categoryUtils->finishLvl3();
+            $lvl3 = false;
+        }
+        $categoryUtils->addLevel2($category);
+    }
+
+    if ($category->lvl3) {
+        $lvl3 = true;
+        $categoryUtils->addLevel3($category);
+    }
+
+    if (preg_match("#Economie#", $category->lvl1)) {
+        break;
+    }
+}
+
+
+//$offres = $hadesRepository->getOffres();
+//dump($offres);
+
 
 get_footer();
