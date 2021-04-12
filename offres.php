a624d06b16564e6401fb42abfe6b36267c3dec99
jfsenechal <jfsenechal@gmail.com>
2021-04-12 11:52:11 +0200

    fix url

--- offres.php
+++ offres.php
@@ -35,12 +35,15 @@ foreach ($categories as $category) {
     $i++;
 }
 $currentUrl = Router::getCurrentUrl();
+$category = get_category_by_slug('offres');
+$categoryUrl = get_category_link($category);
 Twig::rendPage(
     'offre/list.html.twig',
     [
         'url' => '',
         'currentUrl' => $currentUrl,
         'categories' => $categories,
+        'categoryUrl' => $categoryUrl,
     ]
 );
 
