1e655fd9903588d86b25ea5d14af10d54166acb4
jfsenechal <jfsenechal@gmail.com>
2021-04-12 12:01:06 +0200

    cleaning

--- offres.php
+++ offres.php
@@ -6,34 +6,19 @@
 namespace AcMarche\Theme;
 
 use AcMarche\Common\Router;
-use AcMarche\Pivot\Repository\HadesRemoteRepository;
-use AcMarche\Pivot\Repository\HadesRepository;
 use AcMarche\Pivot\Utils\CategoryUtils;
 use VisitMarche\Theme\Lib\Twig;
 
 get_header();
 
-$hadesRemoteRepository = new HadesRemoteRepository();
 $categoryUtils = new CategoryUtils();
-$hadesRepository = new HadesRepository();
-$notEmpty = isset($_GET['notempty']) ?? false;
+$categoryUtils->setCounts();
 $categories = $categoryUtils->categories;
-$i = 0;
-foreach ($categories as $category) {
-    if ($category->category_id) {
-        $count = $hadesRepository->countOffres($category->category_id);
-        $category->count = $count;
-    }
-    if ($notEmpty) {
-        if (isset($count) && $count == 0) {
-            unset($categories[$i]);
-        }
-    }
-    if (preg_match("#Economie#", $category->lvl1)) {
-        //break;
-    }
-    $i++;
+$notEmpty = isset($_GET['notempty']) ?? false;
+if ($notEmpty) {
+    $categories = $categoryUtils->getCategoriesNotEmpty();
 }
+
 $currentUrl = Router::getCurrentUrl();
 $category = get_category_by_slug('offres');
 $categoryUrl = get_category_link($category);
