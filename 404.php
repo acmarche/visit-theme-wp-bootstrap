28eeea6692aae5c07530364fa3aa65cbdf6026df
jfsenechal <jfsenechal@gmail.com>
2021-04-14 10:14:48 +0200

    clean

--- 404.php
+++ 404.php
@@ -6,11 +6,6 @@ use AcMarche\Common\Router;
 use VisitMarche\Theme\Lib\Twig;
 
 get_header();
-global $wp_query;
-dump($wp_query);
-
-global $wp_rewrite;
-dump($wp_rewrite);
 
 $url = Router::getCurrentUrl();
 Twig::rendPage(
