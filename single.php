b28546f0de5db18b754c535e8cfe2f3b76dd865d
jfsenechal <jfsenechal@gmail.com>
2021-04-15 15:07:50 +0200

    fix current cat

--- single.php
+++ single.php
@@ -9,6 +9,7 @@ use VisitMarche\Theme\Lib\WpRepository;
 get_header();
 global $post;
 
+$slugs = explode('/',get_query_var('category_name'));
 $image = null;
 if (has_post_thumbnail()) {
     $images = wp_get_attachment_image_src(get_post_thumbnail_id(), 'original');
@@ -17,7 +18,7 @@ if (has_post_thumbnail()) {
     }
 }
 $wpRepository = new WpRepository();
-$currentCategory = get_category_by_slug(get_query_var('category_name'));
+$currentCategory = get_category_by_slug($slugs[array_key_last($slugs)]);
 $urlBack = get_category_link($currentCategory);
 
 $tags = $wpRepository->getTags($post->ID);
