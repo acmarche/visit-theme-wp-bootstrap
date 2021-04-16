98337be87d53968e0a88f0f2786db8cb0dd0f421
jfsenechal <jfsenechal@gmail.com>
2021-04-16 14:45:51 +0200

    refactor filtres

--- category.php
+++ category.php
@@ -20,26 +20,73 @@ $permalink = get_category_link($cat_ID);
 
 $wpRepository = new WpRepository();
 $categoryUtils = new HadesFiltres();
+$translator = LocaleHelper::iniTranslator();
+$language = LocaleHelper::getSelectedLanguage();
 
-$children = $wpRepository->getChildrenOfCategory($cat_ID);
-if(count($children) > 0) {
-
-}
-
-
-
-$posts = $wpRepository->getPostsByCatId($cat_ID);
 $parent = $wpRepository->getParentCategory($cat_ID);
 
-$translator = LocaleHelper::iniTranslator();
-
-$language = LocaleHelper::getSelectedLanguage();
 $urlBack = '/'.$language;
 $nameBack = $translator->trans('menu.home');
+
 if ($parent) {
     $urlBack = get_category_link($parent->term_id);
     $nameBack = $parent->name;
 }
+
+$children = $wpRepository->getChildrenOfCategory($cat_ID);
+if (count($children) > 0) {
+
+    Twig::rendPage(
+        'category/select_child.html.twig',
+        [
+            'title' => $category->name,
+            'category' => $category,
+            'urlBack' => $urlBack,
+            'nameBack' => $nameBack,
+            'children' => $children,
+        ]
+    );
+
+    get_footer();
+    return;
+}
+
+$filtres = $categoryUtils->getCategoryFilters($cat_ID);
+if (count($filtres) > 0) {
+    $hadesRepository = new HadesRepository();
+    $offres = $hadesRepository->getOffres(array_keys($filtres));
+    array_map(
+        function ($offre) use ($cat_ID) {
+            $offre->url = RouterHades::getUrlOffre($offre, $cat_ID);
+        },
+        $offres
+    );
+
+    wp_enqueue_script(
+        'react-app',
+        get_template_directory_uri().'/assets/js/build/offre.js',
+        array('wp-element'),
+        wp_get_theme()->get('Version'),
+        true
+    );
+
+    Twig::rendPage(
+        'category/index_hades.html.twig',
+        [
+            'category' => $category,
+            'filtres' => $filtres,
+            'offres' => $offres,
+            'title' => $title,
+            'permalink' => $permalink,
+        ]
+    );
+
+    get_footer();
+
+    return;
+}
+
+$posts = $wpRepository->getPostsByCatId($cat_ID);
 
 Twig::rendPage(
     'category/index.html.twig',
@@ -49,7 +96,8 @@ Twig::rendPage(
         'urlBack' => $urlBack,
         'nameBack' => $nameBack,
         'posts' => $posts,
-        'url' => '',
+        'url2' => '',
+        'children' => $children,
     ]
 );
 
