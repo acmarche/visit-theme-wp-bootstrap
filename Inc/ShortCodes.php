197f07a509e301233149f9bac8c9795eb515e018
jfsenechal <jfsenechal@gmail.com>
2021-04-29 21:02:35 +0200

    finish elevation

--- Inc/ShortCodes.php
+++ Inc/ShortCodes.php
@@ -4,7 +4,6 @@ namespace VisitMarche\Theme\Inc;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use VisitMarche\Theme\Lib\Twig;
-use WP_Query;
 
 class ShortCodes
 {
@@ -27,27 +26,25 @@ class ShortCodes
     {
         $file = $args['file'] ?? null;
         if (!$file) {
-            return '<p>Nom de fichier manquant</p>';
+            return '<p>Nom de fichier manquant syntax  = [gpx_viewer file=VTTBleu]</p>';
         }
-        //https://visit.marche.be/wp-content/uploads/2021/04/VTTBleu.gpx
+
         $args = array(
-            // 'posts_per_page' => 1,
             'post_type' => 'attachment',
             'name' => trim($file),
         );
 
         $attachments = get_posts($args);
-        dump($attachments);
-        if (!$attachments || !isset($attachments->posts, $attachments->posts[0])) {
-            return false;
+        if (!$attachments || count($attachments) === 0) {
+            return '<p>Gpx  non trouvé : '.$file.'</p>';
         }
 
-        dump($attachments[0]);
+        $attachment = $attachments[0];
+        $link = $attachment->guid;
 
         $twig = Twig::LoadTwig();
         $post = get_post();
         $title = $post ? $post->post_title : '';
-        $file = '/wp-content/themes/visitmarche/assets/elevation/VTTBleu.gpx';
 
         return $twig->render(
             'map/_gpx_viewer.html.twig',
@@ -55,10 +52,8 @@ class ShortCodes
                 'title' => $title,
                 'latitude' => 50.2268,
                 'longitude' => 5.3442,
-                'file' => $file,
+                'file' => $link,
             ]
         );
     }
-
-
 }
