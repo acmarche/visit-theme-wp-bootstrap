db6dbcf9f65c1344a4ee2adc9489f20b354eb333
jfsenechal <jfsenechal@gmail.com>
2021-05-17 16:38:33 +0200

    redir

--- page-108.php
+++ page-108.php
@@ -7,9 +7,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use VisitMarche\Theme\Lib\Twig;
 
 try {
-    new RedirectResponse('/fr');
-
-    return;
+    return  new RedirectResponse('/fr');
 } catch (\InvalidArgumentException $exception) {
     Mailer::sendError('redirect fr', 'visit marche');
     get_header();
