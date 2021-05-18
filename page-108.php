a408ee09ac353e28143adf92821a3aa4068022ee
jfsenechal <jfsenechal@gmail.com>
2021-05-18 22:37:16 +0200

    clean

--- page-108.php
+++ page-108.php
@@ -1,5 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="refresh" content="0;url='/fr'" />
 
+    <title>Redirecting to /fr</title>
+</head>
+<body>
+Redirecting to <a href="/fr">/fr</a>.
+</body>
+</html>
+
+<?php
+/*
 namespace AcMarche\Theme;
 
 use AcMarche\Common\Mailer;
