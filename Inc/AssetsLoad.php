419659c386746cd4ce6a5af247235de32b0a55f9
jfsenechal <jfsenechal@gmail.com>
2021-05-18 23:26:24 +0200

    decouvrir

--- Inc/AssetsLoad.php
+++ Inc/AssetsLoad.php
@@ -13,7 +13,7 @@ class AssetsLoad
             //  add_action('wp_enqueue_scripts', [$this, 'loadSearchScreenHome']);
         }
 
-        var_dump("zeze".get_queried_object_id());
+        var_dump(get_post());
 
         if (get_queried_object_id() == 828) {
             add_action('wp_enqueue_scripts', [$this, 'visitmarcheHome']);
