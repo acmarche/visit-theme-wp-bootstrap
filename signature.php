72c56154565e609a8714e306b984c48199b49239
jfsenechal <jfsenechal@gmail.com>
2021-09-14 14:20:23 +0200

    signature

---
+++ signature.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Home-Page-Principal
+ */
+
+namespace AcMarche\Theme;
+
+use VisitMarche\Theme\Lib\Twig;
+
+Twig::rendPage(
+    'signatures/heidi.html.twig',
+    [
+
+    ]
+);
