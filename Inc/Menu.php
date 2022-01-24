c11eea236c4cf981eb2a4f525c3927715c218657
jfsenechal <jfsenechal@gmail.com>
2022-01-24 15:01:41 +0100

    cache

--- Inc/Menu.php
+++ Inc/Menu.php
@@ -51,7 +51,7 @@ class Menu
         $language = LocaleHelper::getSelectedLanguage();
 
         return $this->cache->get(
-            'menu_top_4'.$language,
+            'menu_top_4'.$language.time(),
             function () {
                 $menu = [
                     'sorganiser' => get_category_by_slug('sorganiser'),
