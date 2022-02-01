da205da5d53636e8c9b672c47b57ade769390e61
jfsenechal <jfsenechal@gmail.com>
2022-02-01 14:20:19 +0100

    set count listing debug

--- offres.php
+++ offres.php
@@ -6,17 +6,19 @@
 namespace AcMarche\Theme;
 
 use AcMarche\Pivot\Repository\HadesRepository;
-use VisitMarche\Theme\Lib\HadesFiltresTheme;
+use VisitMarche\Theme\Lib\HadesFiltresListing;
 use VisitMarche\Theme\Lib\Router;
 use VisitMarche\Theme\Lib\Twig;
 
 get_header();
 
-$categoryUtils = new HadesFiltresTheme();
+$categoryUtils = new HadesFiltresListing();
 $hadesRepository = new HadesRepository();
-//$tree = $hadesRepository->getTree();
-//$categoryUtils->setCounts($tree);
-$filters = isset($_GET['notempty']) ? $categoryUtils->getFiltresNotEmpty() : $hadesRepository->getTree();
+$filters = $hadesRepository->getTree();
+$categoryUtils->setCounts($filters);
+if (isset($_GET['notempty'])) {
+    $categoryUtils->getFiltresNotEmpty($filters);
+}
 
 $currentUrl = Router::getCurrentUrl();
 $category = get_category_by_slug('offres');
