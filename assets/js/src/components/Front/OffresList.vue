af353f72036c7cd3d2ffab22c90defb95fdac243
jfsenechal <jfsenechal@gmail.com>
2022-08-17 11:12:47 +0200

    v-if="offres.length

--- assets/js/src/components/Front/OffresList.vue
+++ assets/js/src/components/Front/OffresList.vue
@@ -5,7 +5,16 @@ const props = defineProps({offres: Array})
 
 </script>
 <template>
-  <ul class="pt-24px pt-md-32px d-md-flex flex-md-wrap mx-md-n4px mx-lg-n8px object-cardsList mw-1440px">
+  <ul v-if="offres.length > 0" class="pt-24px pt-md-32px d-md-flex flex-md-wrap mx-md-n4px mx-lg-n8px object-cardsList mw-1440px">
+    <OffreItem
+        v-for="(offre, index) in offres"
+        :offre="offre"
+        :index="index"
+        :key="offre.id"
+    >
+    </OffreItem>
+  </ul>
+  <ul v-if="offres.length === 0" class="pt-24px pt-md-32px d-md-flex flex-md-wrap mx-md-n4px mx-lg-n8px object-cardsList mw-1440px">
     <OffreItem
         v-for="(offre, index) in offres"
         :offre="offre"
