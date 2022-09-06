05f1d12a5fd906b735df6ceeb4e2deb976ac7d2e
jfsenechal <jfsenechal@gmail.com>
2022-09-06 14:14:42 +0200

    test vue js messageValue

--- assets/js/src/AppAdmin.vue
+++ assets/js/src/AppAdmin.vue
@@ -3,6 +3,7 @@ import {ref, onMounted} from 'vue'
 import {fetchFiltresByCategoryRequest} from './service/filtre-service'
 import ListFiltre from './components/Admin/ListFiltre.vue';
 import AddFilter from "./components/Admin/AddFilter.vue";
+import My from "./components/Admin/My.vue";
 
 const filtres = ref([])
 const categoryId = ref(0)
@@ -16,9 +17,14 @@ onMounted(async () => {
   categoryId.value = Number(document.getElementById('filtres-box').getAttribute('data-category-id'));
   await callback()
 })
+const message = ref('hello')
 </script>
 
 <template>
-    <AddFilter :categoryId="categoryId" @refresh-filtres="callback"/>
-    <ListFiltre :categoryId="categoryId" :filtres="filtres" @refresh-filtres="callback"/>
+  <!--  <My v-model="message" /> {{ message }}  sample modelValue -->
+  <My v-model:message="message"/>
+  {{ message }}
+
+  <AddFilter :categoryId="categoryId" @refresh-filtres="callback"/>
+  <ListFiltre :categoryId="categoryId" :filtres="filtres" @refresh-filtres="callback"/>
 </template>
