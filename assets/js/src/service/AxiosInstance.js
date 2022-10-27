d0fd4c7987facce1f6d75cb4613e1095dc9d8a86
jfsenechal <jfsenechal@gmail.com>
2022-10-27 14:13:12 +0200

    compile

--- assets/js/src/service/AxiosInstance.js
+++ assets/js/src/service/AxiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     //baseURL: 'https://visit.marche.be/'
-    baseURL: 'https://pivot.visitmarche.be/'
+    baseURL: 'https://visitmarche.be/'
 });
 
 export default instance;
