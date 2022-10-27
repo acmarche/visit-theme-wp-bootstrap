665e91f567dc75687f5e89ad313cf64c9c626ffb
jfsenechal <jfsenechal@gmail.com>
2022-10-27 21:50:11 +0200

    add write, remove locale, change idmap

--- Lib/GpxViewer.php
+++ Lib/GpxViewer.php
@@ -14,12 +14,19 @@ class GpxViewer
         $post = get_post();
         $title = $post ? $post->post_title : '';
 
+        try {
+            $this->writeTmpFile($gpx);
+        } catch (\Exception $exception) {
+
+        }
+
         return $twig->render(
             'map/_gpx_viewer.html.twig',
             [
                 'title' => $title,
                 'latitude' => 50.2268,
                 'longitude' => 5.3442,
+                // 'file' => 'https://visit.marche.be/output/gpx.xml',
                 'file' => $gpx->url,
                 'file2' => null,
             ]
@@ -30,7 +37,7 @@ class GpxViewer
     {
         try {
             $filesystem = new Filesystem();
-            $filesystem->dumpFile(sys_get_temp_dir().'/'.'file.gpx', $gpx->data_raw);
+            $filesystem->dumpFile(sys_get_temp_dir().'/'.'file.gpx', file_get_contents($gpx->url));
         } catch (IOExceptionInterface $exception) {
             echo "An error occurred while creating your directory at ".$exception->getPath();
         }
