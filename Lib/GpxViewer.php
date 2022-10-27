6f4402cf0c66e7ed1ad506d2108aedf177c46913
jfsenechal <jfsenechal@gmail.com>
2022-10-27 22:02:15 +0200

    path

--- Lib/GpxViewer.php
+++ Lib/GpxViewer.php
@@ -26,8 +26,8 @@ class GpxViewer
                 'title' => $title,
                 'latitude' => 50.2268,
                 'longitude' => 5.3442,
-                // 'file' => 'https://visit.marche.be/output/gpx.xml',
-                'file' => $gpx->url,
+                'file' => 'https://visit.marche.be/var/gpx.xml',
+                //'file' => $gpx->url,
                 'file2' => null,
             ]
         );
@@ -37,7 +37,8 @@ class GpxViewer
     {
         try {
             $filesystem = new Filesystem();
-            $filesystem->dumpFile(sys_get_temp_dir().'/'.'file.gpx', file_get_contents($gpx->url));
+            //$filesystem->dumpFile(sys_get_temp_dir().'/'.'file.gpx', file_get_contents($gpx->url));
+            $filesystem->dumpFile('/homez.1029/visitmp/www/var/gpx.xml', file_get_contents($gpx->url));
         } catch (IOExceptionInterface $exception) {
             echo "An error occurred while creating your directory at ".$exception->getPath();
         }
